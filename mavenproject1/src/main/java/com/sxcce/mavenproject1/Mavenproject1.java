488ee6a3f16d1339ff58b6f8ee5e69ff7b1b651d
shajashan26 <shaja.502248@sxcce.edu.in>
2025-01-31 11:25:02 +0530

    Add files via upload

---
+++ mavenproject1/src/main/java/com/sxcce/mavenproject1/Mavenproject1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.sxcce.mavenproject1;
+
+/**
+ *
+ * @author IT20
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        int first = 10;
+    int second = 20;
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println(first + " + " + second + " = "  + sum);
+    }
+}
